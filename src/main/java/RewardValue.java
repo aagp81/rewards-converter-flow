948055274edf6d8712be8dc700a710c5ff1f1d15
Adolfo Gonzalez <aagp81@yahoo.es>
2023-12-05 10:45:10 +0800

    Fix error

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,16 @@
 public class RewardValue {
 
     private double cashValue;
-    private double milesValue;
 
     // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = convertCashToMiles(cashValue);
     }
 
     // Constructor that accepts a value in miles
-    public RewardValue(double milesValue, boolean isMiles) {
-        if (isMiles) {
-            this.milesValue = milesValue;
-            this.cashValue = convertMilesToCash(milesValue);
-        } else {
-            // Handle the case where isMiles is false (for example, set default values)
-            this.milesValue = 0;
-            this.cashValue = 0;
-        }
+    public RewardValue(int milesValue) {
+        this.cashValue = convertMilesToCash(milesValue);
+       
     }
 
     // Getter method for cash value
@@ -28,16 +20,16 @@ public class RewardValue {
 
     // Getter method for miles value
     public double getMilesValue() {
-        return milesValue / 0.0035;
+        return convertCashToMiles(this.cashValue);
     }
 
     // Convert cash to miles at a rate of 0.0035
-    private double convertCashToMiles(double cashValue) {
-        return cashValue * 0.0035;
+    private int convertCashToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
     }
 
     // Convert miles to cash at a rate of 0.0035
-    private double convertMilesToCash(double milesValue) {
-        return milesValue / 0.0035;
+    private double convertMilesToCash(int milesValue) {
+        return milesValue * 0.0035;
     }
 }
